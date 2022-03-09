95935f3facb97ee3f90754fc9fcf91d14c37be0b
Nathiara-Costa <nathiara@live.com>
2022-03-08 23:18:26 -0300

    Exercicio Laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,51 @@
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////// Exercicio Laços///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+///////////////Exercícios de interpretação de código/////////////////////
+
+        //Exercicio 1 
+/*
+R: O codigo está incrementando valores a variavel valor atraves de um laço for, que no caso do exemplo o for vai executar 4 vezes que é a condição dele. 
+
+
+
+        //Exercicio 2
+A) será impresso na tela todos os numeros maiores que 18. 
+
+B) Com o for of não é possivel identificar o indice do array. Uma solução para isso seria utilizar o for normal conforme o exemplo abaixo:
+
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+
+for(let i = 0; i < lista.length; i++) {
+    console.log("indice " + i + ": " + lista[i])
+  }
+
+   // Exercicio 3 
+O resultado impresso na tela seria: 
+                *
+                **
+                ***
+                ****
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////////Exercícios de escrita de código///////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+    // Exercicio 1
+
+let bichoDeEstimacao = Number(prompt("Insira quantos bichinhos de estimação você tem: ")) 
+if(bichoDeEstimacao <= 0 ){
+    console.log("Que pena! Você pode adotar um pet!")
+}else {
+    let nomes = []
+for(let i=0; i < bichoDeEstimacao; i++) {
+   nomes[i]= prompt("insira o nome do seu bicho")
+
+}
+    console.log(nomes)
+}
+
+        // Exercicio 2 
+*/
+
