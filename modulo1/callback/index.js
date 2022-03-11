ea5560b0ae5fe8fdc772c2877b41b03d919f74b7
Nathiara-Costa <nathiara@live.com>
2022-03-10 21:27:15 -0300

    Exercicio Callback

---
+++ modulo1/callback/index.js
@@ -0,0 +1,27 @@
+/*  //////////////////////////////////////////////////////////////////////////////
+///////////////////Exercícios de interpretação de código//////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+        //Exercicio 1
+
+    
+
+A) apelido: Mandi nome: Amanda Rangel
+   apelido: Laura nome: Laís Petra
+   apelido: Chijo nome: Letícia Chijo
+
+
+
+        //Exercicio 2
+
+A) 0: Amanda Rangel
+   1: Laís Petra
+   2: Letícia Chijo
+
+
+        //Exercicio 3
+
+A) 0: nome: Amanda Rangel, apelido: Mandi
+   1: nome: Laís Petra, apelido: Laura
+
+   */
