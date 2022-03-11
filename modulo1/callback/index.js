f10bd0c688257916c544cc24514b3146e2729728
Nathiara-Costa <nathiara@live.com>
2022-03-11 09:55:00 -0300

    Exercicio de escrita de codigo (atualizção)

--- modulo1/callback/index.js
+++ modulo1/callback/index.js
@@ -24,4 +24,29 @@ A) 0: Amanda Rangel
 A) 0: nome: Amanda Rangel, apelido: Mandi
    1: nome: Laís Petra, apelido: Laura
 
+
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////Exercícios de escrita de código////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+        //Exercicio 1 
+A)
+ const pets = [
+            { nome: "Lupin", raca: "Salsicha"},
+            { nome: "Polly", raca: "Lhasa Apso"},
+            { nome: "Madame", raca: "Poodle"},
+            { nome: "Quentinho", raca: "Salsicha"},
+            { nome: "Fluffy", raca: "Poodle"},
+            { nome: "Caramelo", raca: "Vira-lata"},
+         ]
+
+        let nomesPets = pets.map((pets, indice, Array) => {
+            return pets.nome 
+        })
+
+        console.log(nomesPets)
    */
+ 
+
+            
+        
