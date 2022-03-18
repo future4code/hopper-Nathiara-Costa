5c7c7d5602063cf295d5d1c48b6199a46d3b78da
Nathiara-Costa <nathiara@live.com>
2022-03-18 16:45:46 -0300

    EXERCICIO-07-RESOLVIDO

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -11,41 +11,41 @@ function retornaTamanhoArray(array) {
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  //return array.reverse()
-  let resultado = []
-  let x = array.length-1
+    //return array.reverse()
+    let resultado = []
+    let x = array.length - 1
 
-  for (i = 0; i < array.length; i++){
-      resultado.push(array[x])
-      x--
-  }
-  return resultado
+    for (i = 0; i < array.length; i++) {
+        resultado.push(array[x])
+        x--
+    }
+    return resultado
 }
 
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort(function(a,b){
-      return a - b
-  })
+    return array.sort(function (a, b) {
+        return a - b
+    })
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-//let numerosPares = array.filter(i => i % 2 === 0)
-let numerosPares = []
-let numeroImpares = []
+    //let numerosPares = array.filter(i => i % 2 === 0)
+    let numerosPares = []
+    let numeroImpares = []
 
-for (i = 0; i < array.length; i++) {
-    if (array[i] % 2 === 0) {
-        numerosPares.push(array[i])
-    } else {
-        numeroImpares.push(array[i])
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            numerosPares.push(array[i])
+        } else {
+            numeroImpares.push(array[i])
+        }
     }
-}
 
- return numerosPares
- }
+    return numerosPares
+}
 
 
 // EXERCÍCIO 05 
@@ -61,21 +61,32 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  let maiorNumero = array.reduce((a,b)=> {
-      return Math.max(a,b);
-  })
+    let maiorNumero = array.reduce((a, b) => {
+        return Math.max(a, b);
+    })
 
-  return maiorNumero
+    return maiorNumero
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+    let menorNumero = Math.min(num1, num2)
+    
+    let objeto = {
+        maiorNumero: Math.max(num1, num2)
+        ,maiorDivisivelPorMenor: false
+        ,diferenca: 0
+    };
+    objeto.maiorDivisivelPorMenor = objeto.maiorNumero % menorNumero === 0
+    objeto.diferenca = objeto.maiorNumero - menorNumero
+   
+    return objeto
 }
+
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -85,27 +96,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -115,10 +126,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
